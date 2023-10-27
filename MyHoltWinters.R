577f145491cc57deed9804b43671e69196a3a3c0
JosepPeiro <j.peiroramos@gmail.com>
2023-10-27 11:22:48 +0200

    Solucionados los errores

--- MyHoltWinters.R
+++ MyHoltWinters.R
@@ -1,10 +1,5 @@
 rm(list = ls())
 set.seed(99183992)
-# First we simulate a time serie that will be used for proves
-sequence_math <- 10 * (cos(seq(0,12 * pi, pi/6))) + sqrt(seq(0,12 * pi, pi/6))
-sequence_sim <- sequence_math + rnorm(length(sequence_math), 0, 2)
-st_simulated <- ts(sequence_sim, frequency = 12, start = c(2000, 1))
-#plot(st_simulated)
 
 #Also the data from the exam of last year
 ferrocarril <- c(
@@ -26,62 +21,110 @@ MyHoltWinters <- function(x, alpha = TRUE, beta = TRUE, gamma = TRUE){
     print("The input vector must be a time serie")
     return()
   }
+  optimizer(x)
+}  
   
-  fun.ajuste <- function(serie, freq, alfa, beta, gamma){
+  
+fun.ajuste <- function(serie, freq, alpha, beta, gamma){
+  
+  n <- length(serie)
+  valfitted <- c()
+  
+  L0 <- sum(serie[1:freq + freq]) / freq
+  T0 <- (sum(serie[1:freq + freq]) - sum(serie[1:freq])) / (freq ^ 2)
+  S0 <- (serie[1:freq + freq] - L0)
+  
+  for (i in (2 * freq + 1):n){
     
-    n <- length(serie)
-    valfitted <- c()
-    
-    L0 <- sum(serie[1:freq + freq]) / freq
-    T0 <- (sum(serie[1:freq + freq]) - sum(serie[1:freq])) / (freq ^ 2)
-    S0 <- (serie[1:freq + freq] - L0)
-    
-    for (i in (2 * freq + 1):n){
-      
-      S0ind <- i %% freq
-      if (S0ind == 0){S0ind <- freq}
+    S0ind <- i %% freq
+    if (S0ind == 0){S0ind <- freq}
 
-      pred <- L0 + T0 + S0[S0ind]
-      valfitted <- c(valfitted, pred)
-      
-      L1 <- alpha * (serie[i] - S0[S0ind]) + (1 - alpha) * (L0 + T0)
-      T1 <- beta * (L1 - L0) + (1 - beta) * T0
-      S1 <- gamma * (serie[i] - L1) + (1 - gamma) * S0[S0ind]
-      
-      L0 <- L1
-      T0 <- T1
-      S0[S0ind] <- S1
-    }
+    pred <- L0 + T0 + S0[S0ind]
+    valfitted <- c(valfitted, pred)
     
-    SE <- sum((serie[(2 * freq + 1):n] - valfitted) ^ 2)
-    ajust <- list(valfitted = valfitted, SE = SE)
-    return(ajust)
+    L1 <- alpha * (serie[i] - S0[S0ind]) + (1 - alpha) * (L0 + T0)
+    T1 <- beta * (L1 - L0) + (1 - beta) * T0
+    S1 <- gamma * (serie[i] - L1) + (1 - gamma) * S0[S0ind]
     
-  }
-  fun.optim <- function(vect, data){
-    print(vect)
-    n <- length(data)
-    aj <- fun.ajuste(data[1:(n - 1)], data[n], vect[1], vect[2], vect[3])
-    print(aj$SE)
-    return(aj$SE)
+    L0 <- L1
+    T0 <- T1
+    S0[S0ind] <- S1
   }
   
+  SE <- sum((serie[(2 * freq + 1):n] - valfitted) ^ 2)
+  ajust <- list(valfitted = valfitted, SE = SE)
+  return(ajust)
+  
+}
+
+fun.optim <- function(vect, data){
+  #print(vect)
+  n <- length(data)
+  aj <- fun.ajuste(data[1:(n - 1)], data[n], vect[1], vect[2], vect[3])
+  return(aj$SE)
+}
+  
+optimizer2 <- function(x){
   p <- frequency(x)
+  value_vector <- c()
+  best_value <- Inf
+  par_matrix <- matrix(ncol = 3)
+  best_par <- NULL
+  for (a in seq(0.1, 0.9, by = 0.1)){
+    for (b in seq(0.1, 0.9, by = 0.1)){
+      for (c in seq(0.1, 0.9, by = 0.1)){
+        optimo <- optim(c(a,b,c), fun.optim, data = c(x, p), method = "L-BFGS-B", 
+                        lower = c(0.001, 0.001, 0.001), upper = c(0.999, 0.999, 0.999))
+        par_matrix <- rbind(par_matrix, optimo$par)
+        value_vector <- c(value_vector, optimo$value)
+        if (optimo$value < best_value){
+          best_value <- optimo$value
+          best_par <- optimo$par
+          print("Iter")
+          print(best_value)
+          print(best_par)
+        }
+      }
+    }
+  }
+}
 
-  optimo <- optim(c(0.5,0.5,0.5), fun.optim, data = c(x, p), method = "L-BFGS-B", 
-                  lower = c(0, 0, 0), upper = c(1, 1, 1))
-  
+optimizer <- function(x){
+  p <- frequency(x)
+  value_vector <- c()
+  best_value <- Inf
+  par_matrix <- matrix(ncol = 3)
+  best_par <- NULL
+  optimo <- optim(rep(0.5,3), fun.optim, data = c(x, p), method = "L-BFGS-B", 
+                  lower = c(0.001, 0.001, 0.001), upper = c(0.999, 0.999, 0.999))
+  par_matrix <- rbind(par_matrix, optimo$par)
+  value_vector <- c(value_vector, optimo$value)
   print(optimo)
 }
 
-#MyHoltWinters(ts(ferrocarril))
-MyHoltWinters(st_simulated)
+# First we simulate a time serie that will be used for proves
+sequence_math <- 10 * (cos(seq(0,12 * pi, pi/6))) + seq(0,12 * pi, pi/6) + 10 * sin(seq(0, pi, pi / 12 / 6))
+sequence_sim <- sequence_math + rnorm(length(sequence_math), 0, 2)
+st_simulated <- ts(sequence_sim, frequency = 12, start = c(2000, 1))
+plot(st_simulated)
 
-# mod<-HoltWinters(st_simulated)
-# mod$coefficients
-# mod$alpha
-# mod$beta
-# mod$gamma
+#MyHoltWinters(ts(ferrocarril))
+
+
+mod<-HoltWinters(st_simulated)
+mod$coefficients
+mod$alpha
+mod$beta
+mod$gamma
 # predict(mod, 5)
 # plot(mod)
 # freq = 12
+
+# Bucle
+# Comparar con Holtwinters
+# Representar
+
+
+MyHoltWinters(st_simulated)
+
+#ajuste_f <- fun.ajuste(st_simulated, p, best_par[1], best_par[2], best_par[3])

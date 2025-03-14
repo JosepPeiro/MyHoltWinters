c1f11b29003565320a33473b0e9b521693622d8a
unknown <j.peiroramos@gmail.com>
2025-03-14 14:03:00 +0100

    Commented

--- MyHoltWinters.R
+++ MyHoltWinters.R
@@ -1,9 +1,48 @@
+# Author: Josep Peiró Ramos
+# Student at Universitat de València
+
+# Alternative algorithm for HoltWinters, with a equivalent but not
+# equal behaviour as the base function from stats R package.
+
+# HoltWinters algorithms is a function for forecasting in a single
+# time serie.
+# It has 3 parameters, alpha, beta and gamma, wich describe
+# respectively the dependency with the previous observed value,
+# the dependence with the tendency and the dependency of the observed
+# periodicity
+
+# We should take into account that for this algorithm we need to ignore
+# the values of the whole first period of the training data for
+# allowing it to warm up, so we won't have an estimation for those
+# firts values
+
+# In this algorithm you can set initial parameters, even though
+# they will be used as initial point for the converging algorithm,
+# that works for reaching an optimum combination which minimizes the
+# error with respect the real data.
+# The three parameters must be between 0 and 1. And are only used if
+# optimum is set to FALSE.
+
 MyHoltWinters <- function(x, alpha = TRUE, beta = TRUE, gamma = TRUE,
                           optimum = FALSE){
-  if (!is.ts(x)){
+  # Main function
+  # Receive values, calculate the coefficients for optimizing the error
+  # and return an object (list), with all the components of this analysis
+  
+  # Inputs:
+  ## x: Time serie -> Input data
+  ## alpha: numeric or boolean -> Parameter related with previous value
+  ## beta: numeric or boolean -> Parameter related with tendency
+  ## gamma: numeric or boolean -> Parameter related with seasonality
+  ## optimum: boolean -> If we are interested in global optimum or just local
+  
+  
+  if (!is.ts(x)){ # Identify input data as time serie
     print("The input vector must be a time serie")
     return()
   }
+  
+  # If initial parameters are not given, initialize them to 0.5
   initial = rep(0.5,3)
   if (is.numeric(alpha)){
     initial[1] = alpha
@@ -15,58 +54,76 @@ MyHoltWinters <- function(x, alpha = TRUE, beta = TRUE, gamma = TRUE,
     initial[3] = gamma
   }
   
-  solution <- list()
-  p <- frequency(x)
+  p <- frequency(x) # Frecuency of the time serie
   
   if (optimum){
+    # If optimum = TRUE, a deeper reseach for good parameters is done
     optimo <- optimizer(x)
   }else{
-  optimo <- optim(initial, fun.optim, data = c(x, p), method = "L-BFGS-B", 
-                  lower = c(0.00, 0.00, 0.00), upper = c(1, 1, 1))
+    # Else we start from a initial point and arrive to local optimum
+    optimo <- optim(initial, fun.optim, data = c(x, p), method = "L-BFGS-B", 
+                    lower = c(0.00, 0.00, 0.00), upper = c(1, 1, 1))
   }
   
+  # We make an estimation of the serie based on the calculated parameters
   ajuste <- fun.ajuste(x, p, optimo$par[1], optimo$par[2], optimo$par[3])
-  plot(x)
-  lines(ts(c(rep(NA, 2 * p), ajuste$valfitted),
+  # Results shown
+  plot(x) # Data
+  lines(ts(c(rep(NA, 2 * p), ajuste$valfitted), # Adjust
            start = start(x), frequency = frequency(x)),
-        type = "l", lty=2, col = "red")
+        type = "l", lty=2, col = "red") 
   
   names(optimo$par) <- c('alpha', 'beta', 'gamma')
   
-  solution$x <- x
-  solution$adjust <- ajuste$valfitted
-  solution$fitted <- ajuste$fitted
-  solution$parameters <- optimo$par
-  solution$aplha <- optimo$par[1]
-  solution$beta <- optimo$par[2]
-  solution$gamma <- optimo$par[3]
-  solution$coefficients <- ajuste$coef
-  solution$SSE <- ajuste$SE
+  # We create an object with all the information and what we want to return
+  solution <- list()
+  solution$x <- x # Data
+  solution$adjust <- ajuste$valfitted # Predicted values as a vector
+  solution$fitted <- ajuste$fitted # Each term for the prediction as matrix
+  solution$parameters <- optimo$par # Every parameter joined in a vector
+  solution$aplha <- optimo$par[1] # Alpha
+  solution$beta <- optimo$par[2] # Beta
+  solution$gamma <- optimo$par[3] # Gamma
+  solution$coefficients <- ajuste$coef # Coefficients for the prediction
+  solution$SSE <- ajuste$SE # Error of the prediction during training
   
   return(solution)
 }
 
   
 fun.ajuste <- function(serie, freq, alpha, beta, gamma){
+  # Function for adjusting a time serie based on certain parameters,
+  # ad calculating how this new serie approximates to real values
   
-  n <- length(serie)
-  valfitted <- c()
+  # Inputs:
+  ## serie: Time serie -> Input data
+  ## freq: integer -> Frequency of time serie
+  ## alpha: numeric or boolean -> Parameter related with previous value
+  ## beta: numeric or boolean -> Parameter related with tendency
+  ## gamma: numeric or boolean -> Parameter related with seasonality
+
   
+  n <- length(serie) # Length time serie
+  valfitted <- c() # Storing predicted values
+  
+  # Based of the theoretic component that sustains this algorithm
+  # Initialize components for HoltWinters formula
   L0 <- sum(serie[1:freq + freq]) / freq
   T0 <- (sum(serie[1:freq + freq]) - sum(serie[1:freq])) / (freq ^ 2)
   S0 <- (serie[1:freq + freq] - L0)
   
-  fitted <- matrix(ncol = 4)
+  fitted <- matrix(ncol = 4) # Storing components
   
-  for (i in (2 * freq + 1):n){
+  for (i in (2 * freq + 1):n){ # Period for warming up the algorithm
     
-    S0ind <- i %% freq
-    if (S0ind == 0){S0ind <- freq}
+    S0ind <- i %% freq # Term in a season
+    if (S0ind == 0){S0ind <- freq} # From 1 to freq (not 0 allowed)
 
-    pred <- L0 + T0 + S0[S0ind]
-    valfitted <- c(valfitted, pred)
-    fitted <- rbind(fitted, c(pred, L0, T0, S0[S0ind]))
+    pred <- L0 + T0 + S0[S0ind] # Prediction based on components
+    valfitted <- c(valfitted, pred) # Store prediction
+    fitted <- rbind(fitted, c(pred, L0, T0, S0[S0ind])) # Store components
     
+    # Update components
     L1 <- alpha * (serie[i] - S0[S0ind]) + (1 - alpha) * (L0 + T0)
     T1 <- beta * (L1 - L0) + (1 - beta) * T0
     S1 <- gamma * (serie[i] - L1) + (1 - gamma) * S0[S0ind]
@@ -76,37 +133,64 @@ fun.ajuste <- function(serie, freq, alpha, beta, gamma){
     S0[S0ind] <- S1
   }
   
+  # Add names and structure to fitted components
   colnames(fitted) <- c("Xhat", "Level", "Tendency", "Season")
   fitted <- ts(fitted[-1,], start = start(lag(serie, k = -2 * freq)), frequency = freq)
-               
-  SE <- sum((serie[(2 * freq + 1):n] - valfitted) ^ 2)
-  coefficients <- c(L0, T0, S0)
+  
+  SE <- sum((serie[(2 * freq + 1):n] - valfitted) ^ 2) # Calculate error
+  coefficients <- c(L0, T0, S0) # Final coefficients
   names(coefficients) <- c('Level', 'Tendency', paste('Season', 1:freq))
   
   ajust <- list(valfitted = valfitted, SE = SE, coef = coefficients, fitted = fitted)
-  return(ajust)
+  return(ajust) # Return an object with everything
 }
 
+
 fun.optim <- function(vect, data){
-  #print(vect)
+  # Function used as measure of error for optimizing
+  # Receives data and parameters and returns the error produced by them
+  # Important: the structure is compatible with optim function
+  # from stats R package
+  
+  # Inputs:
+  ## vect: array -> Vector with 3 parameters: alpha, beta, gamma in that
+  ##                order
+  ## data: array -> Vector containing input data and the frequency in the
+  ##                last position
+  
   n <- length(data)
   aj <- fun.ajuste(data[1:(n - 1)], data[n], vect[1], vect[2], vect[3])
   return(aj$SE)
 }
-  
+
+
 optimizer <- function(x){
-  p <- frequency(x)
-  value_vector <- c()
-  best_value <- Inf
-  par_matrix <- matrix(ncol = 3)
-  best_par <- NULL
-  for (a in seq(0.1, 0.9, by = 0.1)){
-    for (b in seq(0.1, 0.9, by = 0.1)){
-      for (c in seq(0.1, 0.9, by = 0.1)){
+  # Function used if we try to avoid local optimums at training or data
+  # This functions still doesn't guarantee the global optimum but carry
+  # out a deeper research for finding a fine tunning of the three parameter
+  # It iterates between several combinations of parameters and improve
+  # them in order to find the best result.
+  
+  # This functions comes from the necessity of a deeper reseach of good
+  # parameters as we could observe how optim function from stats R package
+  # tends very often to local and/or incorrect and/or not desidered
+  # optimums.
+
+  # Inputs:
+  ## x: Time serie -> Input data
+  
+  
+  p <- frequency(x) # Frequency of data
+  best_value <- Inf # Initialize best error value
+  best_par <- NULL # Initialize best combination of parameters
+  for (a in seq(0.1, 0.9, by = 0.1)){ # Values for alpha
+    for (b in seq(0.1, 0.9, by = 0.1)){ # Values for beta
+      for (c in seq(0.1, 0.9, by = 0.1)){ # Values for gamma
+        # Optimizate from every combination of parameters
         optimo <- optim(c(a,b,c), fun.optim, data = c(x, p), method = "L-BFGS-B", 
-                        lower = c(0, 0, 0), upper = c(1, 1, 1))
-        par_matrix <- rbind(par_matrix, optimo$par)
-        value_vector <- c(value_vector, optimo$value)
+                        lower = c(0, 0, 0), upper = c(1, 1, 1)) 
+        
+        # If a new better solution is found, store it as the best
         if (optimo$value < best_value){
           best_value <- optimo$value
           best_par <- optimo$par
@@ -116,5 +200,5 @@ optimizer <- function(x){
   }
   best_solution <- list(value = best_value,
                         par = best_par)
-  return(best_solution)
+  return(best_solution) # Store and return the best solution
 }
--- Proves.R
+++ Proves.R
@@ -1,3 +1,14 @@
+# Author: Josep Peiró Ramos
+# Student at Universitat de València
+
+# Case study of the development of a HoltWinters algorithm with
+# the same theorical base as the original one from the stats R package,
+# but programmed as a optional exercise for a better understanding of
+# how it works.
+
+# In this script there is a study of how this alternative algorithm
+# performs compared with the original one
+
 rm(list = ls())
 set.seed(99183992)
 
@@ -8,7 +19,7 @@ sequence_math <- 10 * (cos(seq(0,24 * pi, pi/6))) + seq(0,24 * pi, pi/6) + 10 * 
 sequence_sim <- sequence_math + rnorm(length(sequence_math), 0, 2)
 st_simulated <- ts(sequence_sim, frequency = 12, start = c(2000, 1))
 
-#Also the data from the exam of last year
+#Also the study case data
 ferrocarril <- c(
   139733896, 144482139, 123561582, 145152679,
   144482993, 146521012, 125713176, 149226028,
@@ -24,12 +35,14 @@ ferrocarril <- c(
 
 ferro<-ts(ferrocarril, frequency = 4)
 
-
+# Here we observe the solution and the parameters that
+# the original HoltWinter algorithm from Rbase (stats package) returns
 mod<-HoltWinters(st_simulated)
 mod$coefficients
 mod$alpha
 mod$beta
 mod$gamma
+plot(mod)
 
 # Bucle Ok
 # Comparar con Holtwinters Ok
@@ -39,23 +52,29 @@ mod$gamma
 #Comparar con los parametros de holtwinters -> No pasa nada, vuelve a los que ya tenia Ok
 #Mirar la otra serie Ok
 
+# Now we see the result of our HoltWinters program
 Mymodelo <- MyHoltWinters(st_simulated)
 Mymodelo$coefficients
 Mymodelo$parameters
+# It can be seen that the result is as accurate as the base HoltWinters
 
-#Comparar con los parametros de holtwinters
+# Now we try to want to see what happens if we initiate our algorythm
+# with the parameters that the base HoltWinters calculate
 Mymodelo.par <- MyHoltWinters(st_simulated, mod$alpha,mod$beta,mod$gamma)
 Mymodelo.par$parameters
+# And as we can see, the final result is equivalent to our model,
+# that means that the issue with differents parameters isn't due to
+# different initializations, it's due to the fact that they converge
+# in differents directions
 
-
+# Error metrics for comparing algorythms: mape, rmse
 mape_Mymodelo <- 100*mean(abs(st_simulated-Mymodelo$fitted[,1])/st_simulated)
 mape_mod <- 100*mean(abs(st_simulated-mod$fitted[,1])/st_simulated)
 rmse_Mymodelo <- sqrt(mean((st_simulated-Mymodelo$fitted[,1])^2))
 rmse_mod <- sqrt(mean((st_simulated-mod$fitted[,1])^2))
 
-
-
-#Quitar los c primeros valores:
+# As both predictions are not the same length, we'll cut first part of the
+# base prediction so, they become more comparable:
 st_red <- window(mod$fitted, start = c(start(mod$fitted)[1] + 1,start(mod$fitted)[2]))
 mape_mod_red <- 100*mean(abs(st_simulated-st_red[,1])/st_simulated)
 rmse_mod_red <- sqrt(mean((st_simulated-st_red[,1])^2))
@@ -73,6 +92,11 @@ rmse_mod_red
 ###############Ferrocarril#####################
 ###############################################
 
+# We'll do the same for the case study dataset
+# A single time serie about railroads, the interpretations
+# of this data is unknown 
+
+# We'll compare both models and see how they perform
 modfer<-HoltWinters(ferro)
 plot(modfer)
 modfer$alpha
@@ -85,4 +109,27 @@ Mymodelofer$parameters
 Mymodelo.par.fer <- MyHoltWinters(ferro, modfer$alpha,modfer$beta,modfer$gamma)
 Mymodelo.par$parameters
 
-#best_Mymodelo <- MyHoltWinters(st_simulated, optimum = T)
+# In this case, the adaptations to training data looks the same confident
+# in both approaches.
+# And we could see how the behaviour or parameters are dependent of their
+# initializations
+# A deeper research in this direction can be done, but it's visual that
+# out algorithm can replace the base one as they perform roughly equivalent
+
+# Now this is different,  but equivalent method from our
+# algorithm for converging
+best_Mymodelo <- MyHoltWinters(ferro, optimum = T)
+best_Mymodelo$aplha
+best_Mymodelo$beta
+best_Mymodelo$gamma
+
+# It is more computationally expensive. And although the
+# theorical component is deeper and more robust, there is not a visual
+# improvement in this study case, as it converges to the same parameters.
+# However, a better study can be done to find if there can be a different
+# behaviour in different dataset
+
+mape_best_Mymodelo <- 100*mean(abs(ferro-best_Mymodelo$fitted[,1])/ferro)
+mape_best_Mymodelo
+rmse_best_Mymodelo <- sqrt(mean((ferro-best_Mymodelo$fitted[,1])^2))
+rmse_best_Mymodelo
